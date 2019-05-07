6b85329b36ee55d439067843eafad2d2527864b0
marcinzelent <zelent.marcin@gmail.com>
2019-05-07 09:32:32 +0200

    Added project with stub functions

---
+++ GradeConverter/GradeConverter.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeConverter
+{
+    public static class GradeConverter
+    {
+        public static String ConvertToEuropean(String grade)
+        {
+            return "";
+        }
+
+        public static String ConvertToDanish(String grade)
+        {
+            return "";
+        }
+    }
+}
