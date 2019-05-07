967a930f0f6ed6b8c566c74c82dd24019108a868
marcinzelent <zelent.marcin@gmail.com>
2019-05-07 09:35:46 +0200

    Added working code

--- GradeConverter/GradeConverter.cs
+++ GradeConverter/GradeConverter.cs
@@ -5,14 +5,40 @@ namespace GradeConverter
 {
     public static class GradeConverter
     {
+        static String[,] conversionTable = {
+            {"A", "12"},
+            {"B", "10"},
+            {"C", "7"},
+            {"D", "4"},
+            {"E", "2"},
+            {"FX", "0"},
+            {"F", "-2"}
+        };
+
         public static String ConvertToEuropean(String grade)
         {
-            return "A";
+            String output = "";
+
+            for (int i = 0; i < conversionTable.GetLength(0); i++)
+            {
+                if (grade == conversionTable[i,1])
+                    output = conversionTable[i,0];
+            }
+
+            return output;
         }
 
         public static String ConvertToDanish(String grade)
         {
-            return "12";
+            String output = "";
+
+            for (int i = 0; i < conversionTable.GetLength(0); i++)
+            {
+                if (grade == conversionTable[i,0])
+                    output = conversionTable[i,1];
+            }
+
+            return output;
         }
     }
 }
